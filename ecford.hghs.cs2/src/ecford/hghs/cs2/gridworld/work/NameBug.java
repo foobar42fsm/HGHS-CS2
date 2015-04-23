51e35ae89c10d0fadb532f8598f93dc7fb5b5d30
Elliot Ford <webelliot@fordcochrane.com>
2015-04-23 08:31:03 -0400

    initial commit of NameBug and NameBugs

---
+++ ecford.hghs.cs2/src/ecford/hghs/cs2/gridworld/work/NameBug.java
@@ -0,0 +1,15 @@
+package ecford.hghs.cs2.gridworld.work;
+
+import info.gridworld.actor.Bug;
+
+import java.awt.Color;
+
+public class NameBug extends Bug {
+
+	public NameBug(Color color) {
+		super(color);
+	}
+	
+	
+
+}
