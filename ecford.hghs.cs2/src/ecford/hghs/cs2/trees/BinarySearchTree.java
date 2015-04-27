ce4043760a1dcfd76c90dd0c834ef0ea5b8a6b11
Elliot Ford <webelliot@fordcochrane.com>
2015-04-27 08:30:46 -0400

    intial commit BinarySearchTree

---
+++ ecford.hghs.cs2/src/ecford/hghs/cs2/trees/BinarySearchTree.java
@@ -0,0 +1,47 @@
+package ecford.hghs.cs2.trees;
+
+import java.util.Comparator;
+
+public class BinarySearchTree<E> extends BinaryTree {
+
+	public BinarySearchTree() {
+		super();
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public BinarySearchTree(E value) {
+		super(value);
+	}
+	
+	//TODO doesn't work, make this WORK!
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void insert(Comparable add) {
+		TreeNode<E> temp = getRoot();
+		while (temp.getLeft() != null || temp.getRight() != null) {
+			if (add.compareTo(temp.getValue()) > 0) {
+
+			}else {
+				TreeNode<E> input = new TreeNode<E>(, left, right)
+			}
+		}
+	}
+
+	public TreeNode find(Comparable key) {
+		TreeNode p = getRoot();
+		while (p != null && key.compareTo(p.getValue()) != 0) {
+			if (key.compareTo(p.getValue()) < 0) {
+				p = p.getLeft();
+			} else {
+				p = p.getRight();
+			}
+		}
+		return p;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
