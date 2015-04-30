1ff7ffecef4891d614657529fc5870cc9e8e3637
Elliot Ford <webelliot@fordcochrane.com>
2015-04-30 11:50:13 -0400

    edited PostfixEvaluation

--- ecford.hghs.cs2/src/ecford/hghs/cs2/stacks/PostfixEvaluation.java
+++ ecford.hghs.cs2/src/ecford/hghs/cs2/stacks/PostfixEvaluation.java
@@ -2,19 +2,30 @@ package ecford.hghs.cs2.stacks;
 
 public class PostfixEvaluation {
 	private String postfix;
-	char[] operators;
+	private String prefix;
+	private char[] operators;
+	private int value;
 
 	public PostfixEvaluation(String postfix) {
 		super();
 		this.postfix = postfix;
 		operators = new String("+-*/%^").toCharArray();
+		convert();
+	}
+
+	private void convert() {
+		StringBuilder value = new StringBuilder();
+		for (int i = 0, z = postfix.length(); i < postfix.length(); i++, z--) {
+			value.append(postfix.charAt(findDigit(postfix, i)));
+		}
+
 	}
 
 	// preconditions: index value included in search
 	private int findDigit(String input, int index) { // returns the position in
 														// the string, -1 if
 														// index to big
-		if (input.length() < index) {
+		if (input.length() <= index) {
 			return -1;
 		}
 		for (int i = index; i < input.length(); i++) {
@@ -28,7 +39,7 @@ public class PostfixEvaluation {
 	private int findOperator(String input, int index) { // returns the postition
 														// in the string, -1 if
 														// index to big
-		if (input.length() < index) {
+		if (input.length() <= index) {
 			return -1;
 		}
 		for (int i = index; i < input.length(); i++) {
@@ -43,8 +54,8 @@ public class PostfixEvaluation {
 	private boolean isOperator(char input) {
 		boolean retX = false;
 		for (int i = 0; i < operators.length; i++) {
-			if(input == operators[i]) {
-				
+			if (input == operators[i]) {
+				return true;
 			}
 		}
 		return false;
